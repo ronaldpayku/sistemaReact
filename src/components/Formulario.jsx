afb255748e4b287b786c000e4308bfd32bd004e4
ronaldperaza <ronaldperaza460@gmail.com>
2021-05-04 17:04:51 -0400

    doing test

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,58 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import '../index'
 
 const Formulario = () => {
+
+    const [tokenPublico, setTokenPublico] = useState('');
+    const [tokenPrivado, setTokenPrivado] = useState('');
+    const [codigo, setCodigo] = useState('');
+
+    const [descripcion, setDescripcion] = useState([])
+
+    const enviarSolicitud = (e) => {
+        e.preventDefault();
+        
+        if(!tokenPublico.trim()){
+            console.log('Ingresa el token publico')
+            return
+        }
+        
+        if(!tokenPrivado.trim()){
+            console.log('Ingresa el token privado')
+            return
+        }
+        
+        if(!codigo.trim()){
+            console.log('Ingresa codigo de consulta')
+            return
+        }
+        console.log('procesando datos....')
+
+        setDescripcion([
+            ...descripcion,
+            {
+                campoPublico: tokenPublico,
+                campoPrivado: tokenPrivado,
+                campoCodigo: codigo
+            }
+        ])
+
+        //para limpiar los campos
+        e.target.reset() 
+
+        //para que vuelvan a estar vacios en los hooks
+        setTokenPublico('')
+        setTokenPrivado('') 
+        setCodigo('')
+    }
+
+
     return (
         <Fragment>
             <div className="container">
                 <div className="row">
                     <div className="col-md-8 offset-md-2">
-                        <form className="form">
+                        <form className="form" onSubmit={ enviarSolicitud}>
                             <div className="title mt-5">
                                 <h2>Sistema Interno Payku</h2>
                             </div>
@@ -33,6 +78,7 @@ const Formulario = () => {
                                             type="text"
                                             name="publico"
                                             placeholder="Ingresa Token Publico"
+                                            onChange={ e => setTokenPublico(e.target.value) }
                                             />
                                     </label>
                                 </div>
@@ -43,6 +89,7 @@ const Formulario = () => {
                                             type="text"
                                             name="privado"
                                             placeholder="Ingresa Token Privado"
+                                            onChange={ e => setTokenPrivado(e.target.value) }
                                             />
                                     </label>
                                 </div>
@@ -50,12 +97,14 @@ const Formulario = () => {
                                     <textarea
                                         name="codigo"
                                         placeholder="Ingresa el Codigo aqui"
+                                        onChange={ e => setCodigo(e.target.value) }
                                         />
                                 </div>
                                 <div className="col-md-12">
-                                    <textarea
-                                        name="respuesta"
+                                        <textarea
+                                            name="respuesta"
                                         />
+
                                 </div>
                                 <div className="col-md-12">
                                     <div className="contact-btn gap-2 d-md-flex justify-content-md-end pb-5">
@@ -64,6 +113,16 @@ const Formulario = () => {
                                 </div>
                             </div>
                         </form>
+                        <ul>
+                            {
+                                descripcion.map((item, index) => (
+                                    <li key={index}>
+                                        {item.campoPublico} - {item.campoPrivado} - {item.campoCodigo}
+
+                                    </li>
+                                ))
+                            }
+                        </ul>
                     </div>
                 </div>
             </div>
