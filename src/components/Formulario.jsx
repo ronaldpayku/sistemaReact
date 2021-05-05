fe6ad43ba6fa0feff4e7df4a3ecb019dfd62f4db
ronaldperaza <ronaldperaza460@gmail.com>
2021-05-05 19:18:29 -0400

    second commit

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,127 +1,150 @@
 import React, { Fragment, useState } from 'react'
+import axios from 'axios';
+
+
 import '../index'
 
 const Formulario = () => {
 
-    const [tokenPublico, setTokenPublico] = useState('');
-    const [tokenPrivado, setTokenPrivado] = useState('');
-    const [codigo, setCodigo] = useState('');
+    const handleChange = (e) => {
+        setRequest({
+          ...request,
+          [e.target.name]: e.target.value,
+        });
+    };
 
-    const [descripcion, setDescripcion] = useState([])
-
-    const enviarSolicitud = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        
-        if(!tokenPublico.trim()){
-            console.log('Ingresa el token publico')
-            return
-        }
-        
-        if(!tokenPrivado.trim()){
-            console.log('Ingresa el token privado')
-            return
-        }
-        
-        if(!codigo.trim()){
-            console.log('Ingresa codigo de consulta')
-            return
-        }
-        console.log('procesando datos....')
-
-        setDescripcion([
-            ...descripcion,
-            {
-                campoPublico: tokenPublico,
-                campoPrivado: tokenPrivado,
-                campoCodigo: codigo
-            }
-        ])
-
-        //para limpiar los campos
-        e.target.reset() 
-
-        //para que vuelvan a estar vacios en los hooks
-        setTokenPublico('')
-        setTokenPrivado('') 
-        setCodigo('')
+        console.log('entrando de nuevo')
+        apiRequest(`${request.method}`, `${request.endpoint}`, `${request.codigo}`, `${request.publico}`, `${request.privado}`)
+        .then(axiosRes => setResult(JSON.stringify(axiosRes.data)))
+        .catch(res => console.error(res.response))
     }
 
+    const apiRequest = (method,path,sendData,publicToken,sign="") => {
+        let sendDataCopy = '';
+        if(method === 'post' || method === 'put' || method === 'get' || method === 'delete') {
+          sendDataCopy = {...sendData}
+        sendDataCopy = JSON.parse(sendDataCopy)
+          
+          console.log(sendDataCopy)
+        }
+        return axios({
+          method: `${method}`,
+          url: `${request.url}${path}`, 
+          data: sendDataCopy,
+          headers: {
+            'Content-Type': 'application/json',
+            // Authorization: `Bearer ${publicToken}`,
+            Authorization: `Bearer ${request.publico}`,
+
+            'Sign': sign,  
+          }
+        })
+      }
+    
+
+    const [request, setRequest] = useState('');
+    const [result, setResult] = useState('');
+   
 
     return (
         <Fragment>
             <div className="container">
                 <div className="row">
                     <div className="col-md-8 offset-md-2">
-                        <form className="form" onSubmit={ enviarSolicitud}>
+                        <form className="form" >
                             <div className="title mt-5">
                                 <h2>Sistema Interno Payku</h2>
                             </div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <select className="form-select mb-5 text-color" aria-label="Default select example">
-                                        <option selected>Selecciona Plataforma</option>
-                                        <option value="1">https://des.payku.cl/</option>
-                                        <option value="2">https://app.payku.cl/</option>
+
+                            <div className="col-md-6">
+                                <label>
+                                    Seleciona metodo de solicitud
+                                    <select onChange={ handleChange } name="method" className="form-select mb-5 mt-2 text-color">
+                                        <option selected disabled></option>
+                                        <option value="get">GET</option>
+                                        <option value="post">POST</option>
+                                        <option value="put">PUT</option>
+                                        <option value="delete">DELETE</option>
                                     </select>
-                                </div>
+                                </label>
+                            </div>
+                            <div className="row">
+                                
                                 <div className="col-md-6">
-                                <select className="form-select mb-5 text-color" aria-label="Default select example">
-                                    <option selected>Selecciona el Endpoint</option>
-                                    <option value="1">api/transaction/</option>
-                                    <option value="2">api/transaction/</option>
-                                </select>
+                                    <label>
+                                    Selecciona Plataforma
+                                        <select onChange={ handleChange } name="url" className="form-select mb-5 mt-2 text-color">
+                                            <option selected disabled></option>
+                                            <option value="https://des.payku.cl/">https://des.payku.cl/</option>
+                                            <option value="https://app.payku.cl/">https://app.payku.cl/</option>
+                                        </select>
+                                    </label>
                                 </div>
+                                
+                                <div className="col-md-6">
+                                    <label>
+                                        Selecciona el Endpoint
+                                        <select className="form-select mb-5 mt-2 text-color" onChange={ handleChange } name="endpoint">
+                                            <option selected disabled></option>
+                                            <option value="api/transaction/">api/transaction/</option>
+                                            <option value="api/transaction/">api/transaction/</option>
+                                        </select>                                    
+                                    </label>
+                                </div>
+                                
                                 <div className="col-md-6">
                                     <label>
                                         Token Publico
                                         <input
+                                            className="mt-2"
                                             type="text"
                                             name="publico"
                                             placeholder="Ingresa Token Publico"
-                                            onChange={ e => setTokenPublico(e.target.value) }
+                                            onChange={ handleChange }
                                             />
                                     </label>
                                 </div>
+                                
                                 <div className="col-md-6">
                                     <label>
                                         Token Privado
                                         <input
+                                            className="mt-2"
                                             type="text"
                                             name="privado"
                                             placeholder="Ingresa Token Privado"
-                                            onChange={ e => setTokenPrivado(e.target.value) }
+                                            onChange={ handleChange }
                                             />
                                     </label>
                                 </div>
+                                
                                 <div className="col-md-12">
                                     <textarea
                                         name="codigo"
                                         placeholder="Ingresa el Codigo aqui"
-                                        onChange={ e => setCodigo(e.target.value) }
-                                        />
+                                        onChange={ handleChange }
+                                    />
                                 </div>
+                                
                                 <div className="col-md-12">
                                         <textarea
+                                            value={ result }
                                             name="respuesta"
                                         />
 
                                 </div>
+                                
                                 <div className="col-md-12">
                                     <div className="contact-btn gap-2 d-md-flex justify-content-md-end pb-5">
-                                        <button className="me-md-2" type="submit">Enviar Solicitud</button>
+                                        <button className="me-md-2" onClick={ handleSubmit } type="submit">Enviar Solicitud</button>
                                     </div>
                                 </div>
                             </div>
                         </form>
                         <ul>
-                            {
-                                descripcion.map((item, index) => (
-                                    <li key={index}>
-                                        {item.campoPublico} - {item.campoPrivado} - {item.campoCodigo}
-
-                                    </li>
-                                ))
-                            }
+                            
                         </ul>
                     </div>
                 </div>
